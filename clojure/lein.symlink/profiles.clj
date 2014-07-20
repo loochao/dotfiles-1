cb3fd996a9c0e5a010b8630b784b5d08db111aa2
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-07-20 16:40:41 -0400

    nrepl req

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -2,4 +2,5 @@
  {:datomic {:install-location "/Users/daniel/src/datomic"}
   :plugins [[lein-midje "3.0.0"]
             [lein-ancient "0.5.4"]
-            [lein-environ "0.4.0"]]}}
+            [lein-environ "0.4.0"]
+            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
