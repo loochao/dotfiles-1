91d35217fde7b681a42bf4e153d8e415c1efed21
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-08-07 15:48:50 -0400

    upgrade cider nrepl

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -2,4 +2,4 @@
  {:datomic {:install-location "/Users/daniel/src/datomic"}
   :plugins [[lein-midje "3.0.0"]
             [lein-ancient "0.5.4"]
-            [cider/cider-nrepl "0.7.0-SNAPSHOT"]]}}
+            [cider/cider-nrepl "0.7.0"]]}}
