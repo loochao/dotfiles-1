e975b2b9d0557bfe47d7bef16e902747b2913c02
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-08-25 16:50:26 -0400

    tmux updates

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,5 +1,4 @@
 {:user
- {:datomic {:install-location "/Users/daniel/src/datomic"}
-  :plugins [[lein-midje "3.0.0"]
+ {:plugins [[lein-midje "3.0.0"]
             [lein-ancient "0.5.4"]
             [cider/cider-nrepl "0.7.0"]]}}
