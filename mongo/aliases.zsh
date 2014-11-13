554977c323e0f7433f3e9a66bfff869e61fbc3f1
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2014-11-13 08:02:22 -0500

    chruby, tmux  projects

--- mongo/aliases.zsh
+++ mongo/aliases.zsh
@@ -1,2 +1,4 @@
-alias mongod='~/mongodb/bin/mongod --config=/Users/daniel/mongodb/mongodb.conf'
-alias mongo='~/mongodb/bin/mongo'
+#alias mongod='~/mongodb/bin/mongod --config=/Users/daniel/mongodb/mongodb.conf'
+#alias mongo='~/mongodb/bin/mongo'
+
+alias mongod="mongod -f ~/mongodb/mongodb.conf"
