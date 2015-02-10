5d874ea5cdc397d0070732f201b63bda153de501
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2015-02-10 11:59:25 -0500

    docker aliases

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -3,3 +3,11 @@ docker-ip() {
 }
 alias b2d="boot2docker"
 alias b2ds='$(boot2docker shellinit)'
+alias dsa='docker stop `docker ps -a -q`'
+alias drma='docker rm `docker ps -a -q`'
+alias drmi='docker rmi'
+dlsi() {
+  docker images | grep $1 | cut -d " " -f 1
+}
+
+alias drcan='drma && dsa && drmi `dlsi "canvasdocker*"`'
